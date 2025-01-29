343d3db2ed23c9d986ff716e5c79b3fb289fcfbf
Nikita Kononenko <vmeansdev@gmail.com>
2025-01-29 12:38:15 +0100

    Fix forgotten bodyParams for post/put/patch http methods

--- Dependencies/AppHttpKit/Sources/AppHttpKit/Client/Client.swift
+++ Dependencies/AppHttpKit/Sources/AppHttpKit/Client/Client.swift
@@ -10,19 +10,34 @@ public extension Client {
         try await request(.init(method: .get, url: endpoint, queryParams: params, headers: headers)).body
     }
 
-    func post(_ endpoint: String, params: [String: AnyEncodable]? = nil, headers: [String: String]? = nil) async throws -> Data {
-        try await request(.init(method: .post, url: endpoint, queryParams: params, headers: headers)).body
+    func post(
+        _ endpoint: String,
+        queryParams: [String: AnyEncodable]? = nil,
+        bodyParams: AnyEncodable? = nil,
+        headers: [String: String]? = nil
+    ) async throws -> Data {
+        try await request(.init(method: .post, url: endpoint, queryParams: queryParams, bodyParams: bodyParams, headers: headers)).body
     }
 
-    func put(_ endpoint: String, params: [String: AnyEncodable]? = nil, headers: [String: String]? = nil) async throws -> Data {
-        try await request(.init(method: .put, url: endpoint, queryParams: params, headers: headers)).body
+    func put(
+        _ endpoint: String,
+        queryParams: [String: AnyEncodable]? = nil,
+        bodyParams: AnyEncodable? = nil,
+        headers: [String: String]? = nil
+    ) async throws -> Data {
+        try await request(.init(method: .put, url: endpoint, queryParams: queryParams, bodyParams: bodyParams, headers: headers)).body
     }
 
-    func patch(_ endpoint: String, params: [String: AnyEncodable]? = nil, headers: [String: String]? = nil) async throws -> Data {
-        try await request(.init(method: .patch, url: endpoint, queryParams: params, headers: headers)).body
+    func patch(
+        _ endpoint: String,
+        queryParams: [String: AnyEncodable]? = nil,
+        bodyParams: AnyEncodable? = nil,
+        headers: [String: String]? = nil
+    ) async throws -> Data {
+        try await request(.init(method: .patch, url: endpoint, queryParams: queryParams, bodyParams: bodyParams, headers: headers)).body
     }
 
-    func delete(_ endpoint: String, params: [String: AnyEncodable]? = nil, headers: [String: String]? = nil) async throws -> Data {
-        try await request(.init(method: .delete, url: endpoint, queryParams: params, headers: headers)).body
+    func delete(_ endpoint: String, queryParams: [String: AnyEncodable]? = nil, headers: [String: String]? = nil) async throws -> Data {
+        try await request(.init(method: .delete, url: endpoint, queryParams: queryParams, headers: headers)).body
     }
 }
--- Dependencies/AppHttpKit/Sources/AppHttpKit/Model/Request.swift
+++ Dependencies/AppHttpKit/Sources/AppHttpKit/Model/Request.swift
@@ -6,16 +6,23 @@ public struct Request {
     public let url: String
     public let queryParams: [String: AnyEncodable]?
     public let headers: [String: String]?
-    public let bodyParams: AnyEncodable? = nil
+    public let bodyParams: AnyEncodable?
 
     public var contentType: String? {
         headers?["Content-Type"]
     }
 
-    public init(method: Method = .get, url: String, queryParams: [String : AnyEncodable]? = nil, headers: [String : String]? = nil) {
+    public init(
+        method: Method = .get,
+        url: String,
+        queryParams: [String: AnyEncodable]? = nil,
+        bodyParams: AnyEncodable? = nil,
+        headers: [String: String]? = nil
+    ) {
         self.method = method
         self.url = url
         self.queryParams = queryParams
+        self.bodyParams = bodyParams
         self.headers = headers
     }
 }
