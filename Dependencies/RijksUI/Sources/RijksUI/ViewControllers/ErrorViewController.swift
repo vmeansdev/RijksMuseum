8de5faecb95b192ef1972ce0481fad5e2aed7f29
Nikita Kononenko <vmeansdev@gmail.com>
2025-01-29 19:33:12 +0100

    Make ErrorViewController public

--- Dependencies/RijksUI/Sources/RijksUI/ViewControllers/ErrorViewController.swift
+++ Dependencies/RijksUI/Sources/RijksUI/ViewControllers/ErrorViewController.swift
@@ -5,7 +5,7 @@ public struct ErrorViewModel {
     let retryAction: (() -> Void)?
 }
 
-class ErrorViewController: UIViewController {
+public class ErrorViewController: UIViewController {
     private lazy var messageLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.preferredFont(forTextStyle: .title1)
@@ -38,7 +38,7 @@ class ErrorViewController: UIViewController {
     }()
     private let viewModel: ErrorViewModel
 
-    init(viewModel: ErrorViewModel) {
+    public init(viewModel: ErrorViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
         view.backgroundColor = .systemBackground
@@ -46,11 +46,11 @@ class ErrorViewController: UIViewController {
         makeLayout()
     }
 
-    required init?(coder: NSCoder) {
+    public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
     }
